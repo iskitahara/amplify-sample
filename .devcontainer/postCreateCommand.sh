2575152f5282fc8e5d55613ad6deb443072b720a
kitaharari <iskitahara@users.noreply.github.com>
2025-01-13 11:33:19 +0900

    Revert "Feature delete"

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,2 +0,0 @@
-npm install
-npm i @aws-amplify/ui-react aws-amplify @aws-amplify/backend @aws-amplify/backend-cli
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,21 @@
-'use client';
+"use client";
 
-import type { Schema } from '@/amplify/data/resource';
-import outputs from '@/amplify_outputs.json';
-import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from 'aws-amplify';
-import { generateClient } from 'aws-amplify/data';
-import { useEffect, useState } from 'react';
-import './../app/app.css';
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([]);
-
-  const listTodos = () => {
+  function listTodos() {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
@@ -29,9 +25,9 @@ export default function App() {
     listTodos();
   }, []);
 
-  const createTodo = () => {
+  function createTodo() {
     client.models.Todo.create({
-      content: window.prompt('Todo content'),
+      content: window.prompt("Todo content"),
     });
   }
 
@@ -41,15 +37,13 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
-            {todo.content}
-          </li>
+          <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href='https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/'>
+        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
       </div>
